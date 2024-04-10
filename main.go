b7b00c6c14dd10b01b5995c9cf3f324dadba60db
Marcin99b <marcin.bigos99@gmail.com>
2024-04-10 22:30:10 +0200

    example api

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type SetMoodRequest struct {
+	Name string `json:"name" validate:"required"`
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Test")
+	})
+	e.POST("/", func(c echo.Context) (err error) {
+		r := new(SetMoodRequest)
+		if err = c.Bind(r); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, r)
+	})
+	e.Logger.Fatal(e.Start(":8888"))
 }
