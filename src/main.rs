b5f4620768ada73882b4048faafbb2fc96fd6386
Marcin99b <marcin.bigos99@gmail.com>
2024-04-10 19:51:32 +0200

    get args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    println!("{}", args[1])
 }
+
+/*
+Idea
+User set timerange when is focused, tired, bored, distracted etc
+with optional information what he/she was doing (what caused this state)
+
+App save it and try to find pattern when these states occurs mostly
+*/
